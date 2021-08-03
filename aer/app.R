a517eb5f5d08cccf59f66ca56f32cd89f2083b6b
Alex Alekseev <aalexee@icloud.com>
2021-08-03 15:34:26 +0200

    - tried to add the zip download functionality. it works with bibs, but does not work with pdfs, which require access - also, downloading files on the server first results in more and more files on the server - it does not automatically delete the files after you refresh the app. although after some time, it did clear the folder.

--- aer/app.R
+++ aer/app.R
@@ -2,9 +2,9 @@
 # packages ----------------------------------------------------------------
 
 library(shiny)
-library(shinyFiles)
-library(zipR)
-library(bslib)
+# library(shinyFiles)
+# library(zipR)
+# library(bslib)
 library(fs)
 library(tidyverse)
 library(rvest)
@@ -25,39 +25,39 @@ ui <- fluidPage(
         column(
             width = 6, offset = 3
             , h1("AER Batch Downloader")
-            , p(
-                "The app downloads citations and PDFs for"
-                , strong("all")
-                , "the articles in a selected issue of the"
-                , a(
-                    "American Economic Review"
-                    , href = "https://www.aeaweb.org/journals/aer"
-                )
-                , " (except the 'Front Matter')."
-            )
-            , p(
-                "Just paste the URL of an issue you want into the"
-                , code("Issue URL")
-                , "field,"
-                , "select the existing download folder on your computer,"
-                , "choose the desired citation format, and click"
-                , code("Download")
-                , "!"
-            )
-            , p(
-                "To download PDFs, you"
-                , strong("have")
-                , "to have access to them"
-                , "via your organization and be on your"
-                , "organization's network."
-                , "Access using your personal subscription and credentials will, probably, not work."
-            )
-            , hr()
+            # , p(
+            #     "The app downloads citations and PDFs for"
+            #     , strong("all")
+            #     , "the articles in a selected issue of the"
+            #     , a(
+            #         "American Economic Review"
+            #         , href = "https://www.aeaweb.org/journals/aer"
+            #     )
+            #     , " (except the 'Front Matter')."
+            # )
+            # , p(
+            #     "Just paste the URL of an issue you want into the"
+            #     , code("Issue URL")
+            #     , "field,"
+            #     , "select the existing download folder on your computer,"
+            #     , "choose the desired citation format, and click"
+            #     , code("Download")
+            #     , "!"
+            # )
+            # , p(
+            #     "To download PDFs, you"
+            #     , strong("have")
+            #     , "to have access to them"
+            #     , "via your organization and be on your"
+            #     , "organization's network."
+            #     , "Access using your personal subscription and credentials will, probably, not work."
+            # )
+            # , hr()
             , h5("Issue URL")
             , textInput("url", label = NULL, value = "https://www.aeaweb.org/issues/617")
-            , h5("Download folder")
-            , shinyDirButton("directory", "Select", NULL)
-            , verbatimTextOutput("directorypath", placeholder = T)
+            # , h5("Download folder")
+            # , shinyDirButton("directory", "Select", NULL)
+            # , verbatimTextOutput("directorypath", placeholder = T)
             , h5("Citation format")
             , selectInput("cit_format", label = NULL
                           , choices = list(
@@ -69,24 +69,33 @@ ui <- fluidPage(
                           ))
             , actionButton(
                 "download_bibs"
-                , "Download citations"
+                , "Prepare citations"
                 # , icon = icon("download")
                 # , class = "btn-primary"
             )
+            , downloadButton(
+                "download_bibs_zip"
+                , "Download citations"
+                , class = "btn-primary"
+            )
+            , hr()
             , actionButton(
                 "download_pdfs"
-                , "Download PDFs"
+                , "Prepare PDFs"
                 # , icon = icon("download")
                 # , class = "btn-primary"
             )
-            , downloadButton("download_bibs_zip", "Download citations")
+            , downloadButton(
+                "download_pdfs_zip"
+                , "Download PDFs"
+                , class = "btn-primary"
+            )
             , hr()
             , p(
                 "Programmed by "
                 , a(href = "https://aalexee.com", "Alex Alekseev")
                 , "."
             )
-
         )
     )
 )
@@ -97,28 +106,36 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
 
-    volumes <- c(
-        Home = fs::path_home()
-        # , "R Installation" = R.home()
-        , getVolumes()()
-    )
+    # volumes <- c(
+    #     Home = fs::path_home()
+    #     # , "R Installation" = R.home()
+    #     , getVolumes()()
+    # )
+    #
+    # shinyDirChoose(
+    #     input
+    #     , "directory"
+    #     , roots = volumes
+    #     , session = session
+    #     , restrictions = system.file(package = "base")
+    #     , allowDirCreate = FALSE
+    # )
+    #
+    # output$directorypath <- renderText({
+    #     if (is.integer(input$directory)) {
+    #         cat("No directory has been selected (shinyDirChoose)")
+    #     } else {
+    #         parseDirPath(volumes, input$directory)
+    #     }
+    # })
 
-    shinyDirChoose(
-        input
-        , "directory"
-        , roots = volumes
-        , session = session
-        , restrictions = system.file(package = "base")
-        , allowDirCreate = FALSE
-    )
+    # input_directory <- str_c(
+    #     fs::path_home()
+    #     , .Platform$file.sep
+    #     , "shiny"
+    #     )
 
-    output$directorypath <- renderText({
-        if (is.integer(input$directory)) {
-            cat("No directory has been selected (shinyDirChoose)")
-        } else {
-            parseDirPath(volumes, input$directory)
-        }
-    })
+    input_directory <- "/home/shiny/"
 
 
     # download citations ------------------------------------------------------
@@ -151,7 +168,7 @@ server <- function(input, output, session) {
 
                     name_download <- str_c(str_remove_all(i, "\\."), ".", input$cit_format)
                     path <- str_c(
-                        parseDirPath(volumes, input$directory)
+                        input_directory
                         , .Platform$file.sep
                         , name_download
                     )
@@ -189,13 +206,13 @@ server <- function(input, output, session) {
         },
         content = function(zip_file_name) {
             files <- list.files(
-                path = parseDirPath(volumes, input$directory)
+                path = input_directory
                 , full.names = F
             )
             zip::zip(
                 zipfile = zip_file_name
                 , files = files
-                , root = parseDirPath(volumes, input$directory)
+                , root = input_directory
             )
         }
         , contentType = "application/zip"
@@ -226,13 +243,13 @@ server <- function(input, output, session) {
         withProgress(
             expr = {
 
-                for (j in 1:length(articles)) {
+                for (j in 1:2) {
 
                     i <- articles[j]
 
                     name_download <- str_c(str_remove_all(i, "\\."), ".pdf")
                     path <- str_c(
-                        parseDirPath(volumes, input$directory)
+                        input_directory
                         , .Platform$file.sep
                         , name_download
                     )
@@ -258,6 +275,24 @@ server <- function(input, output, session) {
             , message = "Downloaded"
         )
     })
+
+    output$download_pdfs_zip <- downloadHandler(
+        filename = function() {
+            "pdfs.zip"
+        },
+        content = function(zip_file_name) {
+            files <- list.files(
+                path = input_directory
+                , full.names = F
+            )
+            zip::zip(
+                zipfile = zip_file_name
+                , files = files
+                , root = input_directory
+            )
+        }
+        , contentType = "application/zip"
+    )
 
 }
 
